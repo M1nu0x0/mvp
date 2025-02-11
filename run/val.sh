e2879ef6cdbb8298dd0a574e9f9ea471c7431bb0
M1nu0x0 <oxygen3112@daum.net>
2025-02-11 21:03:29 +0900

    중간저장

--- run/val.sh
+++ run/val.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 torchrun --nproc_per_node=1 --standalone ./run/validate_3d.py --cfg configs/panoptic/best_model_config.yaml --model_path models/model_best_5view.pth.tar
+# python -m torch.distributed.launch --nproc_per_node=1 --use_env run/validate_3d.py --cfg configs/panoptic/best_model_config.yaml --model_path models/model_best_5view.pth.tar
